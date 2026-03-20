4922928c47c134f859fd69d57464db7aaa0c715e
gp1547416-coder <gp1547416@gmail.com>
2026-03-20 11:06:23 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        trevor: "#2563eb",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
